165baee98892011391b33257b4285e1f21099ab6
chrisziegler <chrisrziegler@gmail.com>
2018-11-06 12:25:42 -0800

    Added styled-components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import './App.css';
 import Signin from './signin';
 import Counter from './counter';
 import useSigninStatus from './status';
+import styled from 'styled-components';
+
+const Header = styled.h1`
+  color: midnightblue;
+  font-size: 1.6rem;
+`;
+
+const Span = styled.span`
+  font-style: italic;
+  filter: brightness(130%);
+`;
 
 function App(props) {
   // destructure the 2 elements off useState passed an initial value of 0
@@ -22,7 +33,10 @@ function App(props) {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <Header>
+        React <Span>v16.7.0-alpha</Span> Hooks Demo
+      </Header>
+      <div className="App-header">
         <Signin {...status} />
         <Counter
           isSignin={status.isSignin}
@@ -30,7 +44,7 @@ function App(props) {
           Increment={Incvalue}
           Decrement={Decvalue}
         />
-      </header>
+      </div>
     </div>
   );
 }
--- src/counter.js
+++ src/counter.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  margin-right: 10px;
+  /* Adapt the colors based on primary prop */
+  background: ${props => (props.primary ? 'orangered' : 'white')};
+  color: ${props => (props.primary ? 'white' : 'orangered')};
+`;
 
 export default ({ isSignin, value, Increment, Decrement }) => {
   if (isSignin) {
     return (
       <div>
         <h1>{value}</h1>
-        <button onClick={Increment}>Increment</button>
-        <button onClick={Decrement}>Decrement</button>
+        <Button primary onClick={Increment}>
+          Increment
+        </Button>
+        <Button onClick={Decrement}>Decrement</Button>
       </div>
     );
   }
